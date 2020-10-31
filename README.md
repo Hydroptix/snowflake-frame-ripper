3445681b2215a824fb29b67d0063b98222e5059b
hydroptix <samuelfrazee@hotmail.com>
2020-10-31 10:57:49 -0700

    Add comments and add command-line arguments w/README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # snowflake-frame-ripper
+
+Find unique frames from videos for training models.
+
+## Usage
+ `python3 frame-ripper.py [-t threshold] video_folder_path`
+ 
+### video_folder_path
+
+The tool will find all .mp4 files (shallowly) in the given directory. The folder path must be the last argument in the command.
+
+### -t threshold
+
+The float value below which a frame will be counted as unique. For the dataset I originally used. The similarity value is given using the TM_COEFF_NORM metric in OpenCV2's template matching function.
+
+For my dataset, I found the right ratio of "unique" to "non-unique" frames to with a threshold value of 0.17, and that's the default for this program. Your mileage may vary, especially since OpenCV's template matching really can't tell when an image is being rotated. The template matching function gives tons of digits of precision, so be as precise as you like.
--- frame-ripper.py
+++ frame-ripper.py
@@ -1,8 +1,10 @@
+import getopt
 import cv2
 from pathlib import Path
 from os import mkdir, cpu_count
 from tqdm import tqdm
 from multiprocessing import Pool
+from sys import argv
 
 DIFFERENCE_THRESHOLD = 0.17
 
@@ -36,6 +38,7 @@ def get_unique_frames(video_file: str, threshold=DIFFERENCE_THRESHOLD, show_prog
     valid_frame, frame = video.read()
     current_frame_count: int = 0
 
+    # Make sure we can actually read the first frame from this video
     if valid_frame:
         try:
             mkdir(images_folder)
@@ -49,27 +52,32 @@ def get_unique_frames(video_file: str, threshold=DIFFERENCE_THRESHOLD, show_prog
     valid_frame, frame = video.read()
     current_frame_count += 1
 
+    # Reset all timestamp vars
     frame_time_ms: int = 0
     frame_time_s: int = 0
     frame_time_m: int = 0
     frame_time_h: int = 0
 
-    pbar = None
     if(show_progress_bar):
         pbar = tqdm(total=total_frames)
+    else:
+        pbar = None
 
     while valid_frame:
+        #Calculate current time from fps and number of frames
         total_time_s = current_frame_count / fps
         frame_time_s = int(total_time_s) % 60
         frame_time_m = int((total_time_s / 60)) % 60
         frame_time_h = int(total_time_s / 360)
         frame_time_ms = int(total_time_s * 1000) % 1000
 
+        # Calculate similarity between the last unique frame and this frame using template matching
         similarity = cv2.matchTemplate(frame, last_unique_frame, cv2.TM_CCOEFF_NORMED)
         _, max_v, _, _ = cv2.minMaxLoc(similarity)
 
         #print(f"max_v:{max_v}")
 
+        # When a unique frame is found, save it as a .png with its timestamp in the file title
         if max_v < threshold:
             print(f"{video_name} {frame_time_h}h{frame_time_m}m{frame_time_s}s{frame_time_ms}ms: {round(max_v * 100, 3)}% similar")
             cv2.imwrite(images_folder.joinpath(f"{video_name}-{frame_time_h}h{frame_time_m}m{frame_time_s}s{frame_time_ms}.png").as_posix(), frame)
@@ -89,4 +97,26 @@ def get_unique_frames(video_file: str, threshold=DIFFERENCE_THRESHOLD, show_prog
     return
 
 if __name__ == "__main__":
-    exit(multiprocess_unique_frames("C:\\Users\\sfrazee\\Downloads\\labelbox_upload"))
+
+    if len(argv) < 2:
+        print('frame-ripper.py [-t threshold] video_folder_path')
+
+    try:
+        opts, args = getopt.getopt(argv, "t:")
+    except getopt.GetoptError:
+        print('frame-ripper.py [-t threshold] video_folder_path')
+        exit(2)
+
+    for opt, arg in opts:
+        if opt == 't':
+            try:
+                DIFFERENCE_THRESHOLD = float(arg)
+            except ValueError:
+                print("Threshold value must be a float")
+                exit(2)
+
+    video_folder = Path(argv[len(argv) - 1])
+    if not video_folder.exists():
+        print(f"could not open \"{video_folder}\"")
+
+    exit(multiprocess_unique_frames(video_folder))
